5f455e8a44cfdcff5e4a206c6f9b92228b31c28e
Prakash Gautam <pranphy@gmail.com>
2016-03-04 21:58:24 +0545

    Add author info in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
+/* Name   : main.cpp
+ * Author : @Madhav Humagain
+ * */
 #include "OGL/GLWindow.h"
 
 int main(int argc, char** argv){
-
     myGLwindow* window1 = new myGLwindow(722,722, "Hello Brother Bro");
-
-
     window1->initWindowGraphics();
-
     window1->showWindow();
-
     window1->loopWindow();
-
-
 }
